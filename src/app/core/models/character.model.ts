f0f14ff8983c05cc05488c7679bfcf1faf95f5ac
Juan Vargas <juandeveloper19@gmail.com>
2022-06-04 13:50:51 -0500

    [ADD] The model for the characters was created

---
+++ src/app/core/models/character.model.ts
@@ -0,0 +1,46 @@
+export interface Character {
+  id: number;
+  name: string;
+  description?: string;
+  modified: string;
+  thumbnail: {
+    path: string;
+    extension: string;
+  };
+  comics?: {
+    available: number;
+    items: [
+      {
+        resourceURI: string;
+        name: string;
+      }
+    ];
+  };
+  series?: {
+    available: number;
+    items: [
+      {
+        resourceURI: string;
+        name: string;
+      }
+    ];
+  };
+  stories?: {
+    available: number;
+    items: [
+      {
+        resourceURI: string;
+        name: string;
+      }
+    ];
+  };
+  events?: {
+    available: number;
+    items: [
+      {
+        resourceURI: string;
+        name: string;
+      }
+    ];
+  };
+}
