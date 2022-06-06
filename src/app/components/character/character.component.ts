6e596bf8024973131239c724f9f182d2dbfa88eb
Juan Vargas <juandeveloper19@gmail.com>
2022-06-06 02:14:06 -0500

    [EDIT] Changes in the class of the character

--- src/app/components/character/character.component.ts
+++ src/app/components/character/character.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ChangeDetectionStrategy,
+  OnDestroy,
+} from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 
 import { CharacterService } from '../../core/services/character.service';
@@ -10,7 +15,7 @@ import { Character } from '../../core/models/character.model';
   styleUrls: ['./character.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CharacterComponent implements OnInit {
+export class CharacterComponent implements OnInit, OnDestroy {
   public tabs = [
     { title: 'omics', icon: 'fa-solid fa-c' },
     { title: 'eries', icon: 'fa-solid fa-s' },
@@ -19,26 +24,34 @@ export class CharacterComponent implements OnInit {
   ];
   public tabSelected: string | undefined;
   public id: number | any;
-  character: Character | Character | any;
+  public character: Character | any;
 
   constructor(
     private characterService: CharacterService,
     private activatedRoute: ActivatedRoute,
     private router: Router
-  ) {
-    this.activatedRoute.params.subscribe((params) => {
-      this.id = params['id'];
-    });
-  }
+  ) {}
 
   ngOnInit(): void {
     this.tabSelected = this.tabs[0].title;
+
+    this.activatedRoute.params.subscribe((params) => {
+      this.id = params['id'];
+    });
 
     this.characterService
       .getCharacter(this.id)
       .subscribe((character: Character) => (this.character = character));
   }
 
+  //? Handling of component disassembly.
+  ngOnDestroy(): void {
+    this.tabSelected = this.tabs[0].title;
+    this.id = undefined;
+    this.character = undefined;
+  }
+
+  //? Methods.
   public onTabSelected(tab: string): void {
     this.tabSelected = tab;
   }
